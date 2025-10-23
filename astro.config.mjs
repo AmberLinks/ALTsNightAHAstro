9b575c61bfa638f7d78695e71fc3e19aef60bb41
hiiragi <hiiragi.game.dev@gmail.com>
2025-10-23 18:56:00 +0900

    Astroにサイトコンテンツを移植

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://alts-night-ah-astro.vercel.app',
+  integrations: [sitemap()] // (reactなどもここに入れる)
+});
